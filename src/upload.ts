d4d6ba85206a2979c3280630ee11ab3a68b5a76b
Vianney HERVY on COMPUTAH <vianney.hervy@etu.inp-n7.fr>
2024-09-16 22:24:39 +0200

    download and file preview are good ! Next step: suppression

--- src/upload.ts
+++ src/upload.ts
@@ -1,11 +1,8 @@
 import { Request } from "express";
-import fs from "fs";
 import multer from "multer";
 import { join } from "path";
 
 const uploadsDir = join(__dirname, "..", "uploads");
-
-fs.mkdirSync(uploadsDir, { recursive: true }); // create "uploads/" if it doesn't exist
 
 const storage = multer.diskStorage({
     destination: function (_req: Request, _file: Express.Multer.File, cb: (error: null, destination: string) => void) {
